b844432228de417aa7e4ee80e8a647d65923679f
marengd <danian.marengo@hva.nl>
2023-03-15 20:36:01 +0100

    Added Structure And Interaction to the Index.html

--- portfolio/js/modules/interaction.js
+++ portfolio/js/modules/interaction.js
@@ -6,14 +6,18 @@ function handleMouseLeave() {
   // Get the current rotation of the cube
   const currentRotation = getComputedStyle(this).transform;
 
-  // Pause the animation
-  this.style.animationPlayState = 'paused';
+  // Pause the rotating animation
+  this.style.animationPlayState = 'paused, running';
 
-  // Apply the current rotation as inline style to keep the cube in its current position
-  this.style.transform = currentRotation;
+  // Remove the current transform after the returnToOriginal animation finishes
+  setTimeout(() => {
+    this.style.transform = '';
+    this.style.animationPlayState = 'paused, paused';
+  }, 500);
 }
 
 // Add event listeners to each cube
 cubes.forEach((cube) => {
   cube.addEventListener('mouseleave', handleMouseLeave);
 });
+

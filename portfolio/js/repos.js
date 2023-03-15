35a6562dffed1260b38eeea318b555e437164bcd
BuildTools <unconfigured@null.spigotmc.org>
2023-03-15 22:23:36 +0100

    Added comments, for a better understanding

--- portfolio/js/repos.js
+++ portfolio/js/repos.js
@@ -2,15 +2,19 @@ import { $ } from "./modules/helper.js";
 import { getGithubData } from "./modules/api.js";
 
 async function CreateCubes() {
+  // Fetch user and repositories data from GitHub API
   const { user, repos } = await getGithubData();
 
+  // Get DOM elements
   const cubeGrid = $('#cube-grid');
   const repositoryTitle = $('#repository-title');
   const repositoryDescription = $('#repository-description');
   const repositoryLanguages = $('#repository-languages');
   const repositoryDetails = $('#repository-details'); // Get the reference to the #repository-details element
 
+  // Show repository information when hovering over a cube
   function showRepositoryInfo(title, description, languages) {
+    // Update the title and description elements with repository data
     repositoryTitle.textContent = title;
     repositoryDescription.textContent = description;
 
@@ -24,17 +28,21 @@ async function CreateCubes() {
       repositoryLanguages.appendChild(li);
     });
 
-    repositoryDetails.classList.add('active'); // Add the .active class when hovering over a cube
+    // Add the .active class when hovering over a cube
+    repositoryDetails.classList.add('active');
   }
 
+  // Clear repository information and remove the .active class when not hovering over a cube
   function clearRepositoryInfo() {
     repositoryTitle.textContent = '';
     repositoryDescription.textContent = '';
     repositoryLanguages.innerHTML = '';
 
-    repositoryDetails.classList.remove('active'); // Remove the .active class when not hovering over a cube
+    // Remove the .active class when not hovering over a cube
+    repositoryDetails.classList.remove('active');
   }
 
+  // Iterate through the repositories and create cubes
   repos.forEach((repo) => {
     const li = document.createElement('li');
     const a = document.createElement('a');
@@ -44,9 +52,11 @@ async function CreateCubes() {
     const cube = document.createElement('div');
     cube.classList.add('cube');
 
+    // Add event listeners to show and clear repository information on mouseover and mouseout events
     cube.addEventListener('mouseover', () => showRepositoryInfo(repo.name, repo.description, repo.languages));
     cube.addEventListener('mouseout', clearRepositoryInfo);
 
+    // Create cube faces
     const faceClasses = ['top', 'bottom', 'left', 'right', 'front', 'back'];
     faceClasses.forEach((faceClass) => {
       const face = document.createElement('div');
@@ -54,6 +64,7 @@ async function CreateCubes() {
       cube.appendChild(face);
     });
 
+    // Add cube to the anchor element and list item, then append it to the cube grid
     a.appendChild(cube);
     li.appendChild(a);
     cubeGrid.appendChild(li);
